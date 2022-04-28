1abfc2a3be183793433a26d3fbcdd3220ef2301c
shrimaj mehta <shrimaj.mehta60@gmail.com>
2022-04-28 08:45:17 +0530

    [add] added all files in the program

---
+++ js/site_properties.js
@@ -0,0 +1,6 @@
+const site_properties = {
+    use_local_storage: "true",
+    home_page: "./home.html",
+    add_employee_payroll_page: "./payroll_form.html",
+    server_url: "http://127.0.0.1:3000/EmployeePayrollDB"
+  };
